686467dccda4833b8c253bca8ff36542ce2e586b
N:U <wisanu.sr@ku.th>
2025-06-13 13:07:23 +0700

    dev+test case

---
+++ etl.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import math
+
+#E
+df = pd.read_csv("sample_products.csv")
+
+#T
+df["unit_price"] = df["unit_price"].apply(math.ceil)
+
+#L
+df.to_csv("output/clean_products.csv", index=False)
---
+++ test_etl.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from datetime import datetime
+
+dfInput = pd.read_csv('sample_products.csv')
+dfOutput = pd.read_csv('output/cleaned_products.csv')
+
+with open('output/test_result.txt', 'a') as f:
+    
+    f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
+
+    if dfOutput.dtypes['unit_price'] == 'int64':
+        f.write("Case 1: Pass\n")
+    else:
+        f.write("Case 1: Fail\n")
